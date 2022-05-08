ba3b181d651452bdc0c0007fc640d648cae36b6f
trevor256 <256trevor@gmail.com>
2022-05-08 17:17:47 -0500

    minecraft script

---
+++ ms.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get update && sudo apt-get upgrade
+sudo apt install openjdk-17-jre
+
+sudo useradd -m -r -U -d /opt/minecraft -s /bin/bash minecraft
+sudo su -l minecraft
+
+wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar
+sudo java -Xms1024M -Xmx2048M -jar server.jar nogui
+perl -pi -e 's/eula=false/eula=true/g' eula.txt
+sudo java -Xms1024M -Xmx2048M -jar server.jar nogui
+
+crontab -e
+@reboot /opt/minecraft/startmc.sh
