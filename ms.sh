e182f176399e1357c89f3e3d8a3128bdaf8d41cd
Trevor <79394682+trevor256@users.noreply.github.com>
2022-05-08 21:27:49 -0500

    Update ms.sh

--- ms.sh
+++ ms.sh
@@ -9,6 +9,3 @@ wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a7
 java -Xms1024M -Xmx2048M -jar server.jar nogui
 perl -pi -e 's/eula=false/eula=true/g' eula.txt
 java -Xms1024M -Xmx2048M -jar server.jar nogui
-
-crontab -e
-@reboot /opt/minecraft/startmc.sh
