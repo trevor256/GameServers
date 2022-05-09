9388f567504da97a427dcc10abf62e068d3bcd92
Trevor <79394682+trevor256@users.noreply.github.com>
2022-05-08 21:35:56 -0500

    Update ms.sh

--- ms.sh
+++ ms.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-apt-get update && sudo apt-get upgrade
-apt install openjdk-17-jre
+sudo apt-get update && sudo apt-get upgrade
+sudo apt install openjdk-17-jre
 
-useradd -m -r -U -d /opt/minecraft -s /bin/bash minecraft
-su -l minecraft
+sudo useradd -m -r -U -d /opt/minecraft -s /bin/bash minecraft
+sudo su -l minecraft
 
 wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar
 java -Xms1024M -Xmx2048M -jar server.jar nogui
