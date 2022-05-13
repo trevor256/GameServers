22ad1cec8a265cd3e73260f4bb0c0eae4d8691ee
Trevor <79394682+trevor256@users.noreply.github.com>
2022-05-12 20:02:20 -0500

    Update ms.sh

--- ms.sh
+++ ms.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 apt-get update -y && apt-get upgrade -y
 apt install openjdk-17-jre screen
-wget https://storage256.s3.amazonaws.com/minecraft_server/forge-1.18-38.0.17-installer.jar
-java -jar forge-1.18-38.0.17-installer.jar --installServer
-java -Xms1024M -Xmx3096M -jar forge-1.18-38.0.17-installer.jar nogui  
+wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar
+java -Xmx1G -Xms3500M -jar server.jar nogui
 perl -pi -e 's/eula=false/eula=true/g' eula.txt
-java -Xms1024M -Xmx3096M -jar forge-1.18-38.0.17-installer.jar nogui  
+java -Xmx1G -Xms3500M -jar server.jar nogui
