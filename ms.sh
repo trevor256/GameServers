bea2b6161dc9ffc25031c43151f40d4cc41cb8ad
Trevor <79394682+trevor256@users.noreply.github.com>
2022-05-12 21:03:47 -0500

    Update ms.sh

--- ms.sh
+++ ms.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 apt-get update -y && apt-get upgrade -y
-wget  /etc/init.d/
-chmod +x /etc/init.d/startmminecraft.sh
+wget https://raw.githubusercontent.com/trevor256/MinecraftServer/main/StartMinecraft.sh -P /etc/init.d/
+chmod +x /etc/init.d/StartMinecraft.sh
 apt install openjdk-17-jre screen
 wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar
 echo 'eula=true' >> eula.txt
 java -Xmx1024M -Xms1024M -jar server.jar nogui
 
-ufw allow 25565
+#ufw allow 25565
