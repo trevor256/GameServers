dd4720afa6c63279468dbda1c0fc28885828a751
Trevor <79394682+trevor256@users.noreply.github.com>
2022-05-12 20:07:47 -0500

    Update ms.sh

--- ms.sh
+++ ms.sh
@@ -2,6 +2,6 @@
 apt-get update -y && apt-get upgrade -y
 apt install openjdk-17-jre screen
 wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar
-java -Xmx1G -Xms3500M -jar server.jar nogui
+java -Xmx1G -Xms3G -jar server.jar nogui
 perl -pi -e 's/eula=false/eula=true/g' eula.txt
-java -Xmx1G -Xms3500M -jar server.jar nogui
+java -Xmx1G -Xms3G -jar server.jar nogui
