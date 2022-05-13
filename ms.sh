da1a268ae2d2f9926f93d9316bfd5dc7f9cdf6de
Trevor <79394682+trevor256@users.noreply.github.com>
2022-05-12 20:17:15 -0500

    Update ms.sh

--- ms.sh
+++ ms.sh
@@ -2,6 +2,6 @@
 apt-get update -y && apt-get upgrade -y
 apt install openjdk-17-jre screen
 wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar
+echo 'eula=true' >> eula.txt
 java -Xmx1024M -Xms1024M -jar server.jar nogui
-perl -pi -e 's/eula=false/eula=true/g' eula.txt
-java -Xmx1024M -Xms1024M -jar server.jar nogui
+
