15152f75600f816d955db1abde4d60579d7c0185
Trevor <79394682+trevor256@users.noreply.github.com>
2022-05-14 08:23:22 -0500

    Update MineConfig.sh

--- MineConfig.sh
+++ MineConfig.sh
@@ -6,6 +6,6 @@ sudo apt install openjdk-17-jre screen -y
 sudo wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar
 sudo echo 'eula=true' >> eula.txt
 sudo screen -S minecraft
-sudo java -Xmx3096M -Xms1024M -jar server.jar nogui
+sudo java -Xmx3596M -Xms1024M -jar server.jar nogui
 
 #ufw allow 25565
