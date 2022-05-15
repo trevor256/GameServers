b12a50cbc06d520cd9b85664725ef14125cbf90a
trevor256 <256trevor@gmail.com>
2022-05-15 10:49:35 -0500

    test2

--- MineConfig.sh
+++ MineConfig.sh
@@ -2,21 +2,20 @@
 
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo mkdir minecraft
-sudo cd minecraft
 sudo apt install openjdk-17-jre screen ufw rkhunter -y
-sudo wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar
-sudo echo 'eula=true' >> eula.txt
+sudo wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar -P /minecraft
+sudo echo 'eula=true' >> minecraft/eula.txt
 sudo ufw default deny incoming
 sudo ufw allow proto tcp to 0.0.0.0/0 port 22
 sudo ufw allow proto tcp to 0.0.0.0/0 port 25565
 sudo ufw enable -y
+sudo java -Xmx3596M -Xms1024M -jar minecraft/server.jar nogui
 sudo screen -S minecraft
-sudo java -Xmx3596M -Xms1024M -jar server.jar nogui
 
 # auto jobs 
 ######## back up every ?
 # sudo  cp - r -t YYMMDDHHMM world backups/world_backup
-# 
+# ##
 ######## auto start minecraft on start up
 # 
 # echo "#!/bin/bash java -Xmx3596M -Xms1024M -jar server.jar nogui" > start.sh
