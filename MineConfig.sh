ea96b2e196b22df8938ca3725ea09087cca43aae
trevor256 <256trevor@gmail.com>
2022-05-15 11:28:59 -0500

    change

--- MineConfig.sh
+++ MineConfig.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 sudo apt-get update -y && sudo apt-get upgrade -y
-sudo mkdir minecraft
 sudo apt install openjdk-17-jre screen ufw rkhunter -y
-sudo wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar -P /minecraft
-sudo echo 'eula=true' >> minecraft/eula.txt
-sudo ufw default deny incoming
+sudo wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar
+sudo echo 'eula=true' >> eula.txt
 sudo ufw allow proto tcp to 0.0.0.0/0 port 22
 sudo ufw allow proto tcp to 0.0.0.0/0 port 25565
-sudo ufw enable -y
-sudo java -Xmx3596M -Xms1024M -jar minecraft/server.jar nogui
+echo "y" | sudo ufw enable
+sudo java -Xmx1536M -Xms512M -jar server.jar nogui
 #sudo screen -S minecraft
 
 # auto jobs 
