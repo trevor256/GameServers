359bc767d077121f20cd16f89a7a0f943290a43c
Trevor <79394682+trevor256@users.noreply.github.com>
2022-05-23 10:05:57 -0500

    Update MineConfig.sh

--- MineConfig.sh
+++ MineConfig.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt install openjdk-17-jre screen ufw rkhunter -y
 sudo wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar
+sudo wget https://raw.githubusercontent.com/trevor256/MinecraftServer/main/ss.sh
 sudo wget https://raw.githubusercontent.com/trevor256/MinecraftServer/main/script.service -P /etc/systemd/system/
-sudo wget https://raw.githubusercontent.com/trevor256/MinecraftServer/main/script.service
 sudo echo 'eula=true' >> eula.txt
 sudo ufw allow proto tcp to 0.0.0.0/0 port 22
 sudo ufw allow proto tcp to 0.0.0.0/0 port 25565
