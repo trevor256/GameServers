94a6f917bac585561362e5d2d9ae4a3c08bff041
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-06-10 18:04:46 -0500

    1.19

--- ConfigureServer/ConfigMinecraftServer.sh
+++ ConfigureServer/ConfigMinecraftServer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt install openjdk-17-jre screen ufw rkhunter -y
-sudo wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar
+sudo wget https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar
 sudo wget https://raw.githubusercontent.com/trevor256/MinecraftServer/main/ss.sh
 sudo chmod +x ss.sh
 sudo wget https://raw.githubusercontent.com/trevor256/MinecraftServer/main/script.service -P /etc/systemd/system/
