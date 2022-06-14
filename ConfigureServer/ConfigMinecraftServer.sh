77bbe0f3c1ea6cbbbccd8b27d5fcde294c2c3835
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-06-14 16:35:03 -0500

    Update ConfigMinecraftServer.sh

--- ConfigureServer/ConfigMinecraftServer.sh
+++ ConfigureServer/ConfigMinecraftServer.sh
@@ -3,9 +3,9 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt install openjdk-17-jre ufw -y
 sudo wget https://raw.githubusercontent.com/trevor256/MinecraftServer/main/script.service -P /etc/systemd/system/
 sudo wget https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar
-echo -e '#!/bin/bash\nsudo java -Xms1024M -jar server.jar nogui' > start.sh
 echo 'eula=true' >> eula.txt
+echo -e '#!/bin/bash\nsudo java -Xms1024M -jar server.jar nogui' > start.sh
+sudo systemctl enable script.service
 sudo ufw allow proto tcp to 0.0.0.0/0 port 22 25565
 echo "y" | sudo ufw enable
-sudo systemctl enable script.service
 sudo bash start.sh &
