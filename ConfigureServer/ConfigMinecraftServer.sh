ec9b7391cc378269bf549bd0b0ec7657a6c542ae
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-06-14 12:30:57 -0500

    Update ConfigMinecraftServer.sh

--- ConfigureServer/ConfigMinecraftServer.sh
+++ ConfigureServer/ConfigMinecraftServer.sh
@@ -4,7 +4,7 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt install openjdk-17-jre screen ufw -y
 sudo wget https://raw.githubusercontent.com/trevor256/MinecraftServer/main/script.service -P /etc/systemd/system/
 sudo wget https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar
-sudo wget https://raw.githubusercontent.com/trevor256/MinecraftServer/main/ss.sh
+sudo wget https://raw.githubusercontent.com/trevor256/MinecraftServer/main/start.sh
 sudo chmod +x ss.sh
 # configure 
 sudo echo 'eula=true' >> eula.txt
