16cb402f433f1e779cabf66b028326798b5b2d69
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-06-14 19:43:54 -0500

    Update ConfigMinecraftServer.sh

--- ConfigureServer/ConfigMinecraftServer.sh
+++ ConfigureServer/ConfigMinecraftServer.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt install openjdk-17-jre ufw -y
+
+printf "[Unit]
+Description=starts ss.sh on startup
+Wants=network.target
+After=network.target
+
+[Service]
+Nice=5
+KillMode=control-group
+SuccessExitStatus=0 1
+
+ReadWriteDirectories=/home/admin/
+WorkingDirectory=/home/admin/
+ExecStart=/home/admin/start.sh
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/script.service
+
 sudo wget https://raw.githubusercontent.com/trevor256/MinecraftServer/main/script.service -P /etc/systemd/system/
 sudo wget https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar
 echo 'eula=true' >> eula.txt
