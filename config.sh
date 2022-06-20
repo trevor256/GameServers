b3ed2d3a63e04fbcb6c1b31ba2bc38f5380bb0e1
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-06-20 17:39:51 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,9 +6,8 @@
 ########################################
 
 sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install openjdk-17-jre ufw -y ##rminstall Java and firewall
+sudo apt-get install openjdk-17-jre ufw -y #install Java and firewall
 
-echo -e '#!/bin/bash\nsudo cp -u -r world backup\nsudo java -Xms1024M -jar server.jar nogui' > start.sh ##adds script to backup world and run minecraft
 printf "[Unit]
 Description=starts ss.sh on startup
 Wants=network.target
@@ -20,15 +19,16 @@ KillMode=control-group
 SuccessExitStatus=0 1
 ReadWriteDirectories=/home/admin/
 WorkingDirectory=/home/admin/
-ExecStart=/home/admin/start.sh
+ExecStart=/home/admin/run.sh
 
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/script.service 
-sudo systemctl enable script.service ##adds systemd service to run start.sh on boot
+sudo systemctl enable script.service #adds systemd service to run.sh on boot
 
-sudo curl -O https://storage256.s3.amazonaws.com/minecraft_server/downloads/forge-1.19-41.0.42-installer.jar server.jar##download minecraft server
-echo 'eula=true' >> eula.txt ##agree to end user license agreement 
+sudo curl -O https://storage256.s3.amazonaws.com/minecraft_server/downloads/forge-1.19-41.0.42-installer.jar #download minecraft server
+java -jar forge-1.19-41.0.42-installer.jar --installServer
+echo 'eula=true' >> eula.txt #agree to end user license agreement 
 
-sudo ufw allow proto tcp to 0.0.0.0/0 port 22 25565 ##firewall only allows ssh and minecraft
+sudo ufw allow proto tcp to 0.0.0.0/0 port 22 25565 #firewall only allows ssh and minecraft
 echo "y" | sudo ufw enable
-sudo bash start.sh
+sudo bash run.sh
