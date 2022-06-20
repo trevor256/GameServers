81aa447aca776033efb68e7a0e68606bb1f2021b
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-06-20 16:06:27 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt install openjdk-17-jre ufw -y
+sudo apt install openjdk-18-jre ufw -y ##install Java and firewall
 
+echo -e '#!/bin/bash\nsudo cp -u -r world backup\nsudo java -Xms1024M -jar server.jar nogui &' > start.sh ##adds script to backup world and run minecraft
 printf "[Unit]
 Description=starts ss.sh on startup
 Wants=network.target
@@ -16,18 +17,12 @@ WorkingDirectory=/home/admin/
 ExecStart=/home/admin/start.sh
 
 [Install]
-WantedBy=multi-user.target" > /etc/systemd/system/script.service
+WantedBy=multi-user.target" > /etc/systemd/system/script.service 
+sudo systemctl enable script.service ##adds systemd service to run start.sh on boot
 
-sudo wget https://raw.githubusercontent.com/trevor256/MinecraftServer/main/script.service -P /etc/systemd/system/
-sudo wget https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar
-echo 'eula=true' >> eula.txt
-echo -e '#!/bin/bash\nsudo cp -u -r world backup\nsudo java -Xms1024M -jar server.jar nogui ' > start.sh
-# Uploads world Backup to a S3 bucket, specify your bucket location, or ignore for no off server backup
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-# unzip awscliv2.zip
-# sudo ./aws/install
-# 
-sudo systemctl enable script.service
-sudo ufw allow proto tcp to 0.0.0.0/0 port 22 25565
+sudo wget https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar ##download minecraft server
+echo 'eula=true' >> eula.txt ##agree to end user license agreement 
+
+sudo ufw allow proto tcp to 0.0.0.0/0 port 22 25565 ##firewall only allows ssh and minecraft
 echo "y" | sudo ufw enable
 sudo bash start.sh &
