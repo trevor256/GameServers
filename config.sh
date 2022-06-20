0ed93774abb861dbc741866559b1395d6269b660
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-06-20 16:40:17 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -8,7 +8,7 @@
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install openjdk-17-jre ufw -y ##rminstall Java and firewall
 
-echo -e '#!/bin/bash\nsudo cp -u -r world backup\nsudo java -Xms1024M -jar server.jar nogui &' > start.sh ##adds script to backup world and run minecraft
+echo -e '#!/bin/bash\nsudo cp -u -r world backup\nsudo java -Xms1024M -jar server.jar nogui' > start.sh ##adds script to backup world and run minecraft
 printf "[Unit]
 Description=starts ss.sh on startup
 Wants=network.target
@@ -26,9 +26,9 @@ ExecStart=/home/admin/start.sh
 WantedBy=multi-user.target" > /etc/systemd/system/script.service 
 sudo systemctl enable script.service ##adds systemd service to run start.sh on boot
 
-sudo wget https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar ##download minecraft server
+sudo curl -o https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar ##download minecraft server
 echo 'eula=true' >> eula.txt ##agree to end user license agreement 
 
 sudo ufw allow proto tcp to 0.0.0.0/0 port 22 25565 ##firewall only allows ssh and minecraft
 echo "y" | sudo ufw enable
-sudo bash start.sh &
+sudo bash start.sh
