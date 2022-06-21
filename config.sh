010d20a2852d2472ba3d0b49949e7c7327e9e588
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-06-20 19:16:30 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -28,7 +28,11 @@ sudo systemctl enable script.service #adds systemd service to run.sh on boot
 sudo curl -O https://storage256.s3.amazonaws.com/minecraft_server/downloads/forge-1.19-41.0.42-installer.jar #download minecraft server
 java -jar forge-1.19-41.0.42-installer.jar --installServer
 echo 'eula=true' >> eula.txt #agree to end user license agreement 
-
+sudo curl -O https://storage256.s3.amazonaws.com/minecraft_server/downloads/AddStruct.jar
+sudo curl -O https://storage256.s3.amazonaws.com/minecraft_server/downloads/BiomesOPlenty.jar
+sudo curl -O https://storage256.s3.amazonaws.com/minecraft_server/downloads/lithium.jar
+sudo curl -O https://storage256.s3.amazonaws.com/minecraft_server/downloads/sodium.jar
+sudo curl -O https://storage256.s3.amazonaws.com/minecraft_server/downloads/TerraBlender.jar
 sudo ufw allow proto tcp to 0.0.0.0/0 port 22,25565 #firewall only allows ssh and minecraft
 echo "y" | sudo ufw enable
 sudo bash run.sh
