9b76f878fd07d343b789bbd51aaccd76a3379dc4
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-07-31 10:18:11 -0500

    Update config.sh

--- Minecraft/config.sh
+++ Minecraft/config.sh
@@ -26,7 +26,7 @@ WantedBy=multi-user.target" > /etc/systemd/system/startm.service
 
 sudo systemctl enable startm.service
 sudo curl -O https://storage256.s3.amazonaws.com/minecraft_server/downloads/forge-1.19.1-42.0.1-installer.jar
-java -jar forge-1.19-41.0.42-installer.jar --installServer
+java -jar forge-1.19.1-42.0.1-installer.jar --installServer
 echo 'eula=true' >> eula.txt
 sudo wget -P mods https://storage256.s3.amazonaws.com/minecraft_server/downloads/phosphor.jar https://storage256.s3.amazonaws.com/minecraft_server/downloads/AddStruct.jar https://storage256.s3.amazonaws.com/minecraft_server/downloads/lithium.jar 
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
@@ -35,7 +35,7 @@ iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp  -m tcp --dport 25565 -j ACCEPT
 iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
 
-nohup sudo bash run.sh
+sudo bash run.sh
 
 # tar xvf $(/bin/date +%f)Minecraft-Backup.tar.gz world
 # scp backup to a location
