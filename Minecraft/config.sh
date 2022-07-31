dd7a809f55e5cc72497182fd138a68bc524f9797
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-07-31 10:11:36 -0500

    Update config.sh

--- Minecraft/config.sh
+++ Minecraft/config.sh
@@ -25,7 +25,7 @@ ExecStart=/home/admin/run.sh
 WantedBy=multi-user.target" > /etc/systemd/system/startm.service 
 
 sudo systemctl enable startm.service
-sudo curl -O https://storage256.s3.amazonaws.com/minecraft_server/downloads/forge-1.19-41.0.62-installer.jar
+sudo curl -O https://storage256.s3.amazonaws.com/minecraft_server/downloads/forge-1.19.1-42.0.1-installer.jar
 java -jar forge-1.19-41.0.42-installer.jar --installServer
 echo 'eula=true' >> eula.txt
 sudo wget -P mods https://storage256.s3.amazonaws.com/minecraft_server/downloads/phosphor.jar https://storage256.s3.amazonaws.com/minecraft_server/downloads/AddStruct.jar https://storage256.s3.amazonaws.com/minecraft_server/downloads/lithium.jar 
