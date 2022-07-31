b6e2747336fa3b9e5039e7800f9a626e29e63c9e
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-07-31 16:33:37 -0500

    Update config.sh

--- Minecraft/config.sh
+++ Minecraft/config.sh
@@ -5,7 +5,7 @@
 #  Linux OS (ubuntu)
 ########################################
 
-sudo apt update -y && sudo apt update -y
+sudo apt-get update -y && sudo apt-get update 
 sudo apt install openjdk-17-jre-headless -y
 
 printf "[Unit]
@@ -28,14 +28,15 @@ sudo systemctl enable startm.service
 sudo curl -O https://storage256.s3.amazonaws.com/minecraft_server/downloads/forge-1.19.1-42.0.1-installer.jar
 java -jar forge-1.19.1-42.0.1-installer.jar --installServer
 echo 'eula=true' >> eula.txt
-sudo wget -P mods https://storage256.s3.amazonaws.com/minecraft_server/downloads/AddS1.19.jar https://storage256.s3.amazonaws.com/minecraft_server/downloads/XaerosWorldMap1.19.1.jar https://storage256.s3.amazonaws.com/minecraft_server/downloads/lithium-1.19.1.jar https://storage256.s3.amazonaws.com/minecraft_server/downloads/Clumps-forge-1.19.1.jar 
-iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
-iptables -A INPUT -p tcp  -m tcp --dport 25565 -j ACCEPT
-iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
+sudo wget -P mods https://storage256.s3.amazonaws.com/minecraft_server/downloads/AddS1.19.jar https://storage256.s3.amazonaws.com/minecraft_server/downloads/lithium-1.19.1.jar https://storage256.s3.amazonaws.com/minecraft_server/downloads/Clumps-forge-1.19.1.jar 
+sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -p tcp  -m tcp --dport 25565 -j ACCEPT
+sudo iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
 
 sudo bash run.sh &
 
-# tar xvf $(/bin/date +%f)Minecraft-Backup.tar.gz world
-# scp backup to a location
+# aws configure
+# aws s3 cp --recursive world s3://storage256/minecraft_server/backups/world
+# sudo wget -P world https://storage256.s3.amazonaws.com/minecraft_server/
